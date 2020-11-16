6676c76087c9abc9871adbad58f8956c8f1560c0
Alexander <brixzen@hotmail.com>
2020-11-16 17:13:34 +0100

    Documentation

--- classes/Config.php
+++ classes/Config.php
@@ -1,9 +1,29 @@
 <?php
+/*
+General purpose of class:
+    Holds a method that acts as a getter for the globals inside DB.php
+    Abstracts access to the DB.php. Is it a Database wrapper? 
+    Doesn't need to be required anywhere since we have the autoloader.
+*/
 
 #if path is given set $config to the $GLOBALS from init.php
 #break string into array with explode  
 #loop through config for each element of the array that was just exploded
+
+
+
 class Config {
+
+/*
+Name:       get()
+Purpose:    xxx
+How:        $path set to null so that we can check if the path exists
+            $config = $GLOBALS['config']; set config to array that exists inside $GLOBALS['config] from inside init.php
+            $path = explode('/', $path); splits the string being passed from the __construct inside DB.php 
+
+            Methods:    explode()
+            Params:     xxx
+*/
     public static function get($path=null){
         if($path){
             $config = $GLOBALS['config'];
